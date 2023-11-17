ec6c3fd214f755b8454602a69987edde0517e362
andrelambert <rm99148@fiap.com.br>
2023-11-16 22:24:51 -0300

    tornando login obrigatório para acessar a home

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { AuthProvider } from "./contexts/auth";
-
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { AuthProvider} from "./contexts/auth";
+import useAuth from "./hooks/useAuth";
 // Importando componente Navbar
 import Navbar from "./components/Navbar/Navbar";
-
 // Importando Páginas (Routes)
 import Home from "./routes/Home";
 import Error from "./routes/Error"
 import Signup from "./routes/Signup";
 import Signin from "./routes/Signin";
-
 // Importando CSS
 import "./App.css"; // 
 // import 'bootstrap/dist/css/bootstrap.min.css';
+
+const PrivateRoute = ({ element, ...rest }) => {
+  const auth = useAuth();
+
+  return auth.signed ? (
+    element
+  ) : (
+    <Navigate to="/sign-in" replace/>
+  );
+};
 
 const App = () => {
   return (
     <AuthProvider>
       <>
-      <Navbar />
-      <Routes>
-        <Route path="*" element={<Error />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-up" element={<Signup />} />
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="*" element={<Error />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/" element={<PrivateRoute element={<Home />} />} />
+        </Routes>
       </>
     </AuthProvider>  
-  )
-}
+  );
+};
 
 export default App;
--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,10 +1,12 @@
 import { createContext, useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import usersData from "../data/users.json"; // Importe os dados de usuário do arquivo JSON.
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const userToken = localStorage.getItem("user_token");
@@ -26,6 +28,7 @@ export const AuthProvider = ({ children }) => {
         const token = Math.random().toString(36).substring(2);
         localStorage.setItem("user_token", JSON.stringify({ email, token }));
         setUser(storedUser);
+        navigate('/');
         return;
       } else {
         return "E-mail ou senha incorretos";
@@ -55,6 +58,7 @@ export const AuthProvider = ({ children }) => {
   const signout = () => {
     setUser(null);
     localStorage.removeItem("user_token");
+    navigate('/sign-in');
   };
 
   return (
